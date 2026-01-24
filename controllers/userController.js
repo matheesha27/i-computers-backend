23dfd8a65b8007ce79f24e3844f24d45cd8466c6
matheesha27 <matheesha27@gmail.com>
2026-01-24 20:41:37 +0530

    Added Google Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,10 @@
+import axios from "axios";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export function createUser(req, res) {
+
     const data = req.body
 
     // hashing - 10 times hashing (salting rounds)
@@ -14,21 +16,21 @@ export function createUser(req, res) {
     // const user = new User(data)
     const user = new User(
         {
-            email: data.email,
             firstName: data.firstName,
             lastName: data.lastName,
-            password: hashedPassword,
-            role: data.role
+            email: data.email,
+            password: hashedPassword
         }
     )
     user.save().then(()=>{
-        res.json({
+        res.status(201).json({
             message: "User saved successfully"
         })
     })
 }
 
 export function loginUser(req, res) {
+
     const email = req.body.email
     const password = req.body.password
     console.log("Request to login: " + email + ", " + password)
@@ -54,6 +56,7 @@ export function loginUser(req, res) {
                 const isPasswordCorrect = bcrypt.compareSync(password, user.password)
 
                 if(isPasswordCorrect) {
+                    
                     // payload which is to be encrypted
                     const payload = {
                         email: user.email,
@@ -62,24 +65,15 @@ export function loginUser(req, res) {
                         role: user.role,
                         isEmailVerified: user.isEmailVerified,
                         image: user.image
-                    }
-
+                                        }
                     // Generate JWT token
                     const jwtToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: "24h"})
-
-                    res.json({
+                    res.status(200).json({
                         message: "Login Successful",
                         token: jwtToken,
                         role: user.role
                     })
                 } else {
-                    // User.updateOne({email: email}, {
-                    //     invalidTries: user.invalidTries + 1
-                    // }).then(() => {
-                    //     res.json({
-                    //     message: "Invalid Password"
-                    // })
-                    // })
                     res.status(401).json({
                         message: "Invalid Password"
                     })
@@ -100,4 +94,70 @@ export function getUser(req, res) {
     }
 
     res.json(req.user);
+}
+
+export async function googleLogin(req, res) {
+
+    console.log(req.body.token)
+
+    // validate the access token
+    try{
+        const response = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+            headers: {
+                Authorization: `Bearer ${req.body.token}`
+            }
+        })
+        console.log(response.data)
+
+        const user = await User.findOne({email: response.data.email})
+        if (user == null) {
+            const newUser = new User(
+                {
+                    email: response.data.email,
+                    firstName: response.data.given_name,
+                    lastName: response.data.family_name,
+                    password: "123",
+                    image: response.data.picture
+                }
+            )
+            await newUser.save();
+            console.log("Created a new user")
+            const payload = {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role,
+                isEmailVerified: true,
+                image: user.image
+            }
+            const jwtToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: "24h"})
+            res.json({
+                message: "Login Successful",
+                token: jwtToken,
+                role: user.role
+            })
+        } else {
+            console.log("User already exists!")
+            const payload = {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role,
+                isEmailVerified: user.isEmailVerified,
+                image: user.image
+            }
+            // Generate JWT token
+            const jwtToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: "24h"})
+            res.json({
+                message: "Login Successful",
+                token: jwtToken,
+                role: user.role
+            })
+        }
+    } catch(error){
+        res.status(500).json({
+            message: "Google login failed",
+            error: error.message
+        })
+    }
 }
